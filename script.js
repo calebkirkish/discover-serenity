d6a4543678b8f43ce04052d0657e417adccc735d
Josh Zappone <zappone.josh@gmail.com>
2020-07-24 15:30:12 -0700

    completes function to get state/county from lat/lon

--- script.js
+++ script.js
@@ -4,17 +4,34 @@
 
 var gKey = "AIzaSyDQOySmk8taGDt9pVaSXmNHpO0jjMnQkJ8";
 
-
 function getCounty(lat, lon) {
-    var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=47.487983, -121.723172&sensor=false&key=" + gKey
-    $.ajax({
-        method: "GET",
-        url: url
-    }).then(function(response) {
-        console.log(response)
-        // in this retunred object I will need to access the administrative_area_level_2 === county
+  var queryLocation = lat + ", " + lon;
+  var url =
+    "https://maps.googleapis.com/maps/api/geocode/json?latlng=" +
+    queryLocation +
+    "&sensor=false&key=" +
+    gKey;
+  console.log(url);
+  $.ajax({
+    method: "GET",
+    url: url,
+  }).then(function (response) {
+    console.log(response);
+    var components = response.results[0].address_components; //this is an array
+    components.forEach((item) => {
+      var itemState = "";
+      var itemCounty = "";
+      var checkItem = item.types[0];
+      if (checkItem === "administrative_area_level_1") {
+        itemState = item.long_name;
+        console.log(itemState);
+      }
+      if (checkItem === "administrative_area_level_2") {
+        itemCounty = item.long_name;
+        console.log(itemCounty);
+      }
     });
+  });
 }
 
-
-getCounty(1,2)
+getCounty(47.487983, -121.723172);
