0a457415127afffb4a41a3aa5a9a581ba2b6d4d1
Josh Zappone <zappone.josh@gmail.com>
2020-07-25 11:30:08 -0700

    adds error handling for ajx call

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 
 var gKey = "AIzaSyDQOySmk8taGDt9pVaSXmNHpO0jjMnQkJ8";
 
+function locationError(msg) {}
+
 function getCounty(lat, lon) {
   var queryLocation = lat + ", " + lon;
   var url =
@@ -11,12 +13,30 @@ function getCounty(lat, lon) {
     queryLocation +
     "&sensor=false&key=" +
     gKey;
-  console.log(url);
+//   console.log(url);
   $.ajax({
     method: "GET",
     url: url,
+    complete: function (xhr, statusText) { //confirms success
+    //   console.log(xhr.status);
+    },
+    error: function (jqXHR, error, errorThrown) { //if error with call
+      if (jqXHR.status && jqXHR.status == 400) {
+        alert(jqXHR.responseText);
+      } else {
+        // console.log(xhr.status);
+        alert("Something went wrong: " + jqXHR.status);
+      }
+    },
   }).then(function (response) {
-    console.log(response);
+    // console.log(response);
+    if (response.error_message) { // If API invalid or response is 200 but google throwns error
+    //   console.log(response.error_message);
+      alert("Something went wrong with your request.")
+    }
+    if (response.status === "ZERO_RESULTS") {
+        alert("Sorry, we could not find your location")
+    }
     var components = response.results[0].address_components; //this is an array
     components.forEach((item) => {
       var itemState = "";
@@ -24,14 +44,18 @@ function getCounty(lat, lon) {
       var checkItem = item.types[0];
       if (checkItem === "administrative_area_level_1") {
         itemState = item.long_name;
-        console.log(itemState);
+        // console.log(itemState);
       }
       if (checkItem === "administrative_area_level_2") {
         itemCounty = item.long_name;
-        console.log(itemCounty);
+        itemCounty = itemCounty.replace("County", "").trim()
+        // console.log(itemCounty);
       }
+    //   COVIDdata(itemCounty, itemState) --- this will be the function that Caleb is working on.
     });
   });
 }
 
-getCounty(47.487983, -121.723172);
+// Test cases
+// getCounty(0, 0);
+// getCounty(47.487983, -121.723172);
