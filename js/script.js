f861bc2573bb3e4b2ba8910ebb6afaaf6ec2ea6f
Jonathan Martinez <jonathan.martb@gmail.com>
2020-07-25 17:50:17 -0700

    Delete script.js

--- js/script.js
+++
@@ -1,6 +0,0 @@
-// Trail detail modal
-$('.trail-tile').on('click', function() {
-    $('.small.modal')
-    .modal('setting', 'transition', 'vertical flip')
-    .modal('toggle', 'show-dimmer')
-});
